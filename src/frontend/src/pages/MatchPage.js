94dc78a42c0d5c88c2426a237e2d65687b3076d0
Lakshya Seth <sethlakshya98@gmail.com>
2023-09-05 18:59:49 +0530

    Modified match page UI

--- src/frontend/src/pages/MatchPage.js
+++ src/frontend/src/pages/MatchPage.js
@@ -4,10 +4,23 @@ import { MatchSmallCard } from '../components/MatchSmallCard';
 import { useParams } from 'react-router-dom';
 
 export const MatchPage =  () => {
-    
+    const [matches, setMatches] = useState([]); 
+    const { teamName, year } = useParams(); 
+    useEffect(
+        ()=>{
+            const fetchMatches = async () => {
+                const response=await fetch(`http://localhost:8080/team/${teamName}/matches?year=${year}`);
+                const data= await response.json();
+                console.log(data);
+                setMatches(data);
+            }
+            fetchMatches();
+        },[teamName]
+    );
   return (
     <div className="MatchPage">
         <h1>Match Page</h1>
+        {matches.map(match => <MatchDetailCard match={match} teamName={teamName} />)}
     </div>
   );
 }
